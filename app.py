d6dd5bd68e44efa0edaa61215cf668d3839a77c5
yelmokadem <34802470+yelmokadem@users.noreply.github.com>
2023-03-16 21:33:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
     flash("what's your name?")
     return render_template("index.html")
 
-@app.route("/greet", methods = ['POST','GET'])
+@app.route("/greet", methods = 'POST')
 def greet():
     flash("Hi " + str(request.form['name_input'])+ ", great to see you!")
     return render_template("index.html")
